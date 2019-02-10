f1a15c5e5ebf6c2914745b2f5c7ea13427e94226
GreyCat20142017 <nrz3siaatg81@mail.ru>
2019-02-10 13:49:58 +0300

    Задание 9. Уточнение длины имен файлов + немного общепринятой вкусовщины. Пока bet, но возможно, будет stake

--- schema.sql
+++ schema.sql
@@ -6,43 +6,43 @@ USE yeti;
 
 CREATE TABLE categories
 (
-  category_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name        VARCHAR(20)
+  id   INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(20)
 );
 
 CREATE TABLE users
 (
-  user_id           INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id                INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   registration_date TIMESTAMP    NOT NULL        DEFAULT CURRENT_TIMESTAMP,
   email             VARCHAR(128) NOT NULL UNIQUE DEFAULT '',
   name              CHAR(30)     NOT NULL        DEFAULT '',
   user_password     CHAR(20)     NOT NULL        DEFAULT '',
-  avatar            CHAR(128)    NOT NULL        DEFAULT '',
+  avatar            CHAR(32)     NOT NULL        DEFAULT '',
   contacts          TEXT         NOT NULL
 );
 
 CREATE TABLE lots
 (
-  lot_id          INT UNSIGNED   NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id              INT UNSIGNED   NOT NULL AUTO_INCREMENT PRIMARY KEY,
   category_id     INT UNSIGNED   NOT NULL DEFAULT 0,
   owner_id        INT UNSIGNED   NOT NULL DEFAULT 0,
   winner_id       INT UNSIGNED,
   creation_date   TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
   name            CHAR(64)       NOT NULL DEFAULT '',
-  description     VARCHAR(128)   NOT NULL DEFAULT '',
-  image           CHAR(128)      NOT NULL DEFAULT '',
+  description     TEXT           NOT NULL,
+  image           CHAR(50)       NOT NULL DEFAULT '',
   price           DECIMAL(10, 2) NOT NULL DEFAULT 0,
   completion_date DATETIME,
   step            DECIMAL(6, 2)  NOT NULL DEFAULT 0
 );
 
-create table requests
+create table bets
 (
-  request_id   INT UNSIGNED   NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  user_id      INT UNSIGNED   NOT NULL DEFAULT 0,
-  lot_id       INT UNSIGNED   NOT NULL DEFAULT 0,
-  request_date TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  request_sum  DECIMAL(10, 2) NOT NULL DEFAULT 0
+  id             INT UNSIGNED   NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user_id        INT UNSIGNED   NOT NULL DEFAULT 0,
+  lot_id         INT UNSIGNED   NOT NULL DEFAULT 0,
+  placement_date TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  declared_price DECIMAL(10, 2) NOT NULL DEFAULT 0
 );
 
 
