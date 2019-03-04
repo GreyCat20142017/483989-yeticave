2677499d1ebf0dfc5fc9e3132be4058f782ae04d
GreyCat20142017 <nrz3siaatg81@mail.ru>
2019-03-04 23:21:36 +0300

    Удаление забытого комментария

--- init.php
+++ init.php
@@ -1,6 +1,5 @@
 <?php
 
-//    set_include_path(get_include_path() . PATH_SEPARATOR . $_SERVER['DOCUMENT_ROOT'] . '/definitions');
     set_include_path(get_include_path() . PATH_SEPARATOR .  'definitions');
 
     require_once('functions.php');
